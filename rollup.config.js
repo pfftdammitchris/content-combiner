3c6a380150eb28ffab40ba1bc0f58f096325506b
pfftdammitchris <pfftdammitchris@gmail.com>
2020-09-26 15:09:51 -0700

    added babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,38 @@
+import babel from '@rollup/plugin-babel'
 import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import pkg from './package.json'
+// import pkg from './package.json'
 
 export default {
   input: 'src/index.ts',
-  external: ['src/**/*.test.ts'],
+  external: [],
   output: [
+    // {
+    //   file: pkg.main,
+    //   format: 'cjs',
+    //   sourcemap: 'inline-source-map',
+    //   exports: 'named',
+    // },
+    // {
+    //   file: pkg.module,
+    //   format: 'es',
+    //   sourcemap: 'inline-source-map',
+    //   exports: 'named',
+    // },
+
+    // {
+    //   dir: 'dist',
+    //   format: 'cjs',
+    //   sourcemap: 'inline-source-map',
+    //   exports: 'named',
+    // },
     {
-      file: pkg.main,
+      dir: 'dist',
       format: 'cjs',
       sourcemap: 'inline-source-map',
       exports: 'named',
     },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: 'inline-source-map',
-      exports: 'named',
-    },
-    // {
-    //   file: pkg.browser,
-    //   format: 'umd',
-    //   sourcemap: 'inline-source-map',
-    //   name: 'postsAggregator',
-    //   globals: {
-    //     'lodash.get': 'get',
-    //   },
-    //   exports: 'default',
-    // },
   ],
-  plugins: [commonjs(), resolve(), typescript()],
+  plugins: [commonjs(), resolve(), typescript(), babel({ babelHelpers: 'bundled' })],
 }
